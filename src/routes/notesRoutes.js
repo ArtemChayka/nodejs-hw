2f8164db0150046c9c382a04793dff3aac16fe20
Artem Chaychenko <drchaychenko@gmail.com>
2025-10-18 14:37:52 +0300

    8 auth

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { celebrate } from 'celebrate';
 import {
   getAllNotes,
   getNoteById,
@@ -9,21 +8,20 @@ import {
 } from '../controllers/notesController.js';
 import { authenticate } from '../middleware/authenticate.js';
 import {
-  getAllNotesSchema,
-  noteIdSchema,
-  createNoteSchema,
-  updateNoteSchema,
+  validateGetAllNotes,
+  validateNoteId,
+  validateCreateNote,
+  validateUpdateNote,
 } from '../validations/notesValidation.js';
 
 const router = Router();
 
-// Всі маршрути потребують аутентифікації
 router.use(authenticate);
 
-router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
-router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
-router.post('/notes', celebrate(createNoteSchema), createNote);
-router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
-router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+router.get('/notes', validateGetAllNotes, getAllNotes);
+router.get('/notes/:noteId', validateNoteId, getNoteById);
+router.post('/notes', validateCreateNote, createNote);
+router.patch('/notes/:noteId', validateUpdateNote, updateNote);
+router.delete('/notes/:noteId', validateNoteId, deleteNote);
 
 export default router;
